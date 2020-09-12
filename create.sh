e39784344c6f2ba263ceae86fbe5e7de843b2f1d
jctovar <jctovar@gmail.com>
2020-09-12 10:42:13 -0500

    good morning

---
+++ create.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+tns build android
+
+cp /Users/jctovar/Nativescript/psicologia/platforms/android/app/build/outputs/apk/debug/app-debug.apk /Volumes/GoogleDrive/Mi\ unidad/
+mv /Volumes/GoogleDrive/Mi\ unidad/app-debug.apk /Volumes/GoogleDrive/Mi\ unidad/psicologia.apk
